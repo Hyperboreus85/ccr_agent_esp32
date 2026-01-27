ec111fb78cc9fff8789f9cc7b230135f95e27be4
Giacomo V <giacomo4@tiscali.it>
2026-01-27 22:35:14 +0100

    secrets.example.h modified

--- include/secrets.example.h
+++ include/secrets.example.h
@@ -2,15 +2,15 @@
 
 // Copy this file to secrets.h and fill in your credentials.
 
-#define WIFI_SSID "WIFI"
-#define WIFI_PASSWORD "wifi1234"
+#define WIFI_SSID "WIFI-SSID"
+#define WIFI_PASSWORD "WIFI-PASSWORD"
 
 // Optional API key for future server protection.
 // Leave empty string to omit header.
-#define CCR_API_KEY "24b5f9c12e6c7c89a47ad9bc6e9671669f49b6da63f8cbc1cb1c7eda133dc7af"
+#define CCR_API_KEY ""
 
 // Device identifier string
 #define DEVICE_ID "esp32-lab-01"
 
 // Base URL of CCR server (no trailing slash). Example: http://192.168.1.100:3000
-#define CCR_BASE_URL "http://129.152.9.206:3000"
+#define CCR_BASE_URL "http://your-server:3000"
